cd8e7cd3ba21df6fafe51042e5623a3c1917f45b
Lars Graubner <mail@larsgraubner.de>
2016-08-18 08:38:24 +0200

    updates, styled content, add twemoji

--- site/web/app/themes/larsgraubner.de/assets/scripts/modules/common.js
+++ site/web/app/themes/larsgraubner.de/assets/scripts/modules/common.js
@@ -1,8 +1,14 @@
 import Prism from 'prismjs';
+import twemoji from 'twemoji';
 
 export default {
   init() {
     // high light code snippets
     Prism.highlightAll();
+
+    const postContent = document.getElementsByClassName('post__content');
+    if (postContent) {
+      twemoji.parse(postContent[0]);
+    }
   },
 };
