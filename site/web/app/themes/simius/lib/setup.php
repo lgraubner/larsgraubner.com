a9873f3295fd8899c52b5c69be4d0b14d8f12c0f
Lars Graubner <mail@larsgraubner.de>
2016-10-14 11:12:07 +0200

    fix analytics path

--- site/web/app/themes/simius/lib/setup.php
+++ site/web/app/themes/simius/lib/setup.php
@@ -130,7 +130,7 @@ function add_google_analytics() {
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','" . get_home_url('analytics.js', 'https') . "','ga');
+              })(window,document,'script','" . esc_url(home_url('analytics.js', 'https')) . "','ga');
 
                 ga('create', 'UA-44046571-2', 'auto');
                 ga('set', 'anonymizeIp', true);
