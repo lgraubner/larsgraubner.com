4008c3fbe8146ce4df1791fa8c335d9217196700
Lars Graubner <mail@larsgraubner.de>
2016-10-14 11:14:32 +0200

    strip whitespace

--- site/web/app/themes/simius/lib/setup.php
+++ site/web/app/themes/simius/lib/setup.php
@@ -126,7 +126,7 @@ add_action('wp_head', __NAMESPACE__ . '\\js_detection');
 
 function add_google_analytics() {
     if (!is_user_logged_in()) {
-        echo "<script>
+        echo str_replace(array("\r", "\n"), '', "<script>
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -135,7 +135,7 @@ function add_google_analytics() {
                 ga('create', 'UA-44046571-2', 'auto');
                 ga('set', 'anonymizeIp', true);
                 ga('send', 'pageview');
-            </script>";
+            </script>");
     }
 }
 add_action('wp_head', __NAMESPACE__ . '\\add_google_analytics', 10, 1);
