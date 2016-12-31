d8a013846fb344dcf2f02f96e23339c4a1df431a
Lars Graubner <mail@larsgraubner.de>
2016-12-31 15:48:52 +0100

    add dynamic path for favicons

--- site/web/app/themes/simius/templates/head.php
+++ site/web/app/themes/simius/templates/head.php
@@ -10,12 +10,12 @@
 
     <?php wp_head(); ?>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/app/themes/simius/assets/images/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/app/themes/simius/assets/images/favicons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/app/themes/simius/assets/images/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/app/themes/simius/assets/images/favicons/manifest.json">
-    <link rel="mask-icon" href="/app/themes/simius/assets/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="/app/themes/simius/assets/images/favicons/favicon.ico">
-    <meta name="msapplication-config" content="/app/themes/simius/assets/images/favicons/browserconfig.xml">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/manifest.json">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon.ico">
+    <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 </head>
