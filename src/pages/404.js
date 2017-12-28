21e704c014d5c620b90f92df2065723407cc6735
Lars Graubner <mail@larsgraubner.de>
2017-12-28 18:21:57 +0100

    adjust 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,29 @@
 // @flow
 import React from 'react'
-import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import Title from '../components/Title'
 import Text from '../components/Text'
 
+const LimitedText = Text.extend`
+  max-width: 540px;
+  margin-top: 40px;
+`
+
+const SubTitle = styled.h2`
+  font-weight: normal;
+  font-size: 22px;
+  color: rgba(0, 0, 0, 0.8);
+`
+
 const Index = () => (
   <div>
-    <Title>Page Not found</Title>
-    <Text>The requested page was not found.</Text>
-    <Text>
+    <Title>Not found</Title>
+    <SubTitle>The requested page could not be found</SubTitle>
+    <LimitedText>
       Please contact the owner of the site that linked you to the original URL
       and let them know their link is broken.
-    </Text>
+    </LimitedText>
   </div>
 )
 
