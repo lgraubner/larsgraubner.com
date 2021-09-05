d25d0fe294c728eb8f148c0f738d935102f7be9e
Lars Graubner <mail@larsgraubner.de>
2021-09-05 18:00:04 +0200

    feat: install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./layouts/**/*.html', './assets/js/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
